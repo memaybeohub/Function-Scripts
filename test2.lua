353978c1d54d2b675746038acf314fa32a0c3337
memaybeohub <108448173+memaybeohub@users.noreply.github.com>
2023-06-17 10:57:26 +0700

    Add files via upload

--- test2.lua
+++ test2.lua
@@ -242,6 +242,7 @@ function FastAttackConnectorFunction()
                     elseif CountAttack >= FastAttackSettings["CDAAT"] then 
                         print('m3')
                         AttackFunctgggggion()
+                        ToiCanOxi = ToiCanOxi +1
                         if conchosetting["DelayAttack"] and type(conchosetting["DelayAttack"]) == "number" and conchosetting["DelayAttack"] >= 0.1 then 
                             wait(conchosetting["DelayAttack"]*2)
                         else
