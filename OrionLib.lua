edbc0f37c07b343e419a8f8a365d367c2d42f90d
memaybeohub <108448173+memaybeohub@users.noreply.github.com>
2023-06-19 18:24:34 +0700

    Update OrionLib.lua

--- OrionLib.lua
+++ OrionLib.lua
@@ -545,10 +545,10 @@ function OrionLib:MakeWindow(WindowConfig)
 				Size = UDim2.new(0, 32, 0, 32),
 				Position = UDim2.new(0, 10, 0.5, 0)
 			}), {
-				SetProps(MakeElement("Image", "https://media.discordapp.net/attachments/879997074710626374/1119513226355408916/Adel_Hub.png"), {
+				SetProps(MakeElement("Image", "https://tr.rbxcdn.com/7a182b1f9312308fa2ee2597f644050b/420/420/Decal/Png"), {
 					Size = UDim2.new(1, 0, 1, 0)
 				}),
-				AddThemeObject(SetProps(MakeElement("Image", "https://media.discordapp.net/attachments/879997074710626374/1119513226355408916/Adel_Hub.png"), {
+				AddThemeObject(SetProps(MakeElement("Image", "https://tr.rbxcdn.com/7a182b1f9312308fa2ee2597f644050b/420/420/Decal/Png"), {
 					Size = UDim2.new(1, 0, 1, 0),
 				}), "Second"),
 				MakeElement("Corner", 1)
