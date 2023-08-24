25d0dba116ac25e2d7eab77690874da33e31969c
memaybeohub <108448173+memaybeohub@users.noreply.github.com>
2023-08-24 22:31:54 +0700

    Update YMFHUB_MISC.lua

--- YMFHUB_MISC.lua
+++ YMFHUB_MISC.lua
@@ -12,7 +12,7 @@ spawn(function()
     require(game.ReplicatedStorage.Notification).new("<Color=Cyan>To Everyone Who Want Use This Hub!<Color=/>"):Display()
     require(game.ReplicatedStorage.Notification).new("<Color=Orange>If You Wanna Script Update Please:!<Color=/>"):Display()
     require(game.ReplicatedStorage.Notification).new("<Color=Purple>Donate to: MOMO/CARDVIP/TSR: 0395277909 PHAN THI DON/VO MINH HUNG!<Color=/>"):Display()
-    require(game.ReplicatedStorage.Notification).new("<Color=Red>If enough 400K VND (20$) I'll Update More Haha: 0/400<Color=/>"):Display()
+    require(game.ReplicatedStorage.Notification).new("<Color=Red>If enough 400K VND (20$) I'll Update More Haha: 100/400<Color=/>"):Display()
 end)
 spawn(function()
     loadstring(game:HttpGet(
