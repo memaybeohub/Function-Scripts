39ca777fc437021206bb08258ba515b0fb4075b1
memaybeohub <108448173+memaybeohub@users.noreply.github.com>
2024-01-22 22:12:54 +0700

    Create caitao.lua

---
+++ caitao.lua
@@ -0,0 +1,6 @@
+local support = {"Fluxus", "Electron"}
+
+if table.find(support,identifyexecutor())  then
+    return function() print("đúng exploit r chạy thôi uwu") end
+end
+game:GetService("Players").LocalPlayer:Kick("Unknow Exploit | Use Fluxus For Highest Performance.")
